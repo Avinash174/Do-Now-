8941a65d9b9bd3a27aa14657f569ba6eac2faeb3
Avinash175 <avinashmagar15@gmail.com>
2026-03-10 10:33:19 +0530

    fix(android): properly assign subprojects buildDir to fix flutter APK output path

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,13 @@ allprojects {
     }
 }
 
-rootProject.layout.buildDirectory.value(rootProject.layout.buildDirectory.dir("../../build").get())
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
 
 subprojects {
     project.evaluationDependsOn(":app")
